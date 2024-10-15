d9fb41f2df3bd9f1b6ea1eb16b88d2f6616bedde
Sabine Fernandes Kurz <107117180+reallybadbear@users.noreply.github.com>
2024-10-15 10:34:14 -0300

    redesSociaisMinhaEscola.js

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -1,20 +1,60 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+import { criarGrafico, getCSS, incluirTexto } from "./common.js"
 
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
-
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
-
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
+async function redesSociaisFavoritasMinhaEscola() {
+    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+    if (dadosLocaisString) {
+        const dadosLocais = JSON.parse(dadosLocaisString)
+        processarDados(dadosLocais)
+    } else {
+        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+        const res = await fetch(url)
+        const dados = await res.json()
+        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
+        processarDados(dados)
+    }
 }
 
-vizualizarInformacoesGlobais()
+function processarDados(dados) {
+    const redesSociais = dados.slice(1).map(redes => redes[1])
+    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
+        acc[redesSociais] = (acc[redesSociais] || 0) + 1
+        return acc
+    }, {})
+    const valores = Object.values(contagemRedesSociais)
+    const labels = Object.keys(contagemRedesSociais)
+
+    const data = [
+        {
+            values: valores,
+            labels: labels,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
+        title: {
+            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
+            }
+        }
+    }
+
+    criarGrafico(data, layout)
+    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+}
+
+redesSociaisFavoritasMinhaEscola()
